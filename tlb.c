91e1e0cb3d47078617ef400f598b32e7d264a527
pedroLucas25 <pedrolucas111193@hotmail.com>
2018-11-28 06:33:57 -0200

    Push

--- tlb.c
+++ tlb.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <stdint.h>
 
-#define MAX_LINHA 100000
+#define MAX_TEXTO 100000
+#define MAX_ELEMENTOS 10000
 
 uint32_t numPagina(unsigned char*);
 uint32_t numDeslocamento(unsigned char*);
@@ -11,8 +12,8 @@ uint32_t numDeslocamento(unsigned char*);
 int main(int argc, char *argv[]){
 
   FILE *arq;
-  unsigned char texto[MAX_LINHA], address[10][100];
-  int i=0, z;
+  unsigned char texto[MAX_TEXTO], address[MAX_ELEMENTOS][10];
+  unsigned int i, z, j;
 
   arq = fopen(argv[1], "r");
   if (arq == NULL){  // Se houve erro na abertura
@@ -20,9 +21,11 @@ int main(int argc, char *argv[]){
   return EXIT_FAILURE;
   }
 
-  fgets((char*)texto, MAX_LINHA, arq);
+  fgets((char*)texto, MAX_TEXTO, arq);
 
-  for(unsigned int j=0;j<strlen((const char*)texto);j++){
+/////////////////////////////Função que separa os números do arquivo////////////////////////////////////////
+  i = 0;
+  for(j=0;j<strlen((const char*)texto);j++){
     z=0;
     while (texto[i] != ' ') {
       address[j][z] = texto[i];
@@ -31,10 +34,9 @@ int main(int argc, char *argv[]){
     }
     i++;
   }
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  //printf("%s\n", address[3]);
-
-  printf("%x\n", numDeslocamento(address[3]));
+  printf("%s\n", address[2]);
 
   fclose(arq);
   return 0;
